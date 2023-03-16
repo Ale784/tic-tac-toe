f0ecfca3c7da984cdfcc1eb5c97eb11b898d6016
Alejandro <acorrea7412@gmail.com>
2023-03-15 22:49:53 -0500

    Fixed design issue on mobile device

--- src/components/score.jsx
+++ src/components/score.jsx
@@ -4,27 +4,21 @@ export function ScoringBoard (score) {
   const { player, tie, machine } = score.score
 
   return (
-    <div>
-      <section className='winner'>
-        <div className='text'>
-          <section className='score'>
-            <div>
-              <h2>{PLAYERS.player} 1 ({TURNS.X})</h2>
-              <span> {player}</span>
-            </div>
+    <section className='score'>
+      <div className='score__points'>
+        <h2>{PLAYERS.player} 1 ({TURNS.X})</h2>
+        <span> {player}</span>
+      </div>
 
-            <div>
-              <h2>{PLAYERS.tie}</h2>
-              <span>{tie}</span>
-            </div>
+      <div className='score__points'>
+        <h2>{PLAYERS.tie}</h2>
+        <span>{tie}</span>
+      </div>
 
-            <div>
-              <h2>MACHINE ({TURNS.O})</h2>
-              <span>{machine}</span>
-            </div>
-          </section>
-        </div>
-      </section>
-    </div>
+      <div className='score__points'>
+        <h2>MACHINE ({TURNS.O})</h2>
+        <span>{machine}</span>
+      </div>
+    </section>
   )
 }
